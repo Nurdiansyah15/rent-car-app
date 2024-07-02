d44b99218c60dfaddb8c60ea5834fb210a51839e
vigorjs <111201912289@mhs.dinus.ac.id>
2024-07-02 13:21:29 +0700

    feat: update feature balikin

--- src/main/java/enigma/rent/car/app/services/RentServImpl.java
+++ src/main/java/enigma/rent/car/app/services/RentServImpl.java
@@ -1,10 +1,15 @@
 package enigma.rent.car.app.services;
 
+import enigma.rent.car.app.models.Car;
 import enigma.rent.car.app.models.Rent;
+import enigma.rent.car.app.models.User;
+import enigma.rent.car.app.repositories.CarRepo;
 import enigma.rent.car.app.repositories.RentRepo;
+import enigma.rent.car.app.repositories.UserRepo;
 import enigma.rent.car.app.utils.dto.RentDto;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -12,11 +17,15 @@ public class RentServImpl implements RentServ {
     private final RentRepo rentRepository;
     private final UserServ userService;
     private final CarServ carService;
+    private final UserRepo userRepo;
+    private final CarRepo carRepo;
 
-    public RentServImpl(RentRepo rentRepository, UserServ userService, CarServ carServ) {
+    public RentServImpl(RentRepo rentRepository, CarServ carServ, UserServ userService, UserRepo userRepo, CarRepo carRepo) {
         this.rentRepository = rentRepository;
         this.userService = userService;
         this.carService = carServ;
+        this.userRepo = userRepo;
+        this.carRepo = carRepo;
     }
 
 
@@ -33,11 +42,12 @@ public class RentServImpl implements RentServ {
     @Override
     public Rent create(RentDto rent) {
         Rent newRent = new Rent();
+        Car existingCar = carService.findById(rent.getCar_id());
 
         newRent.setUser(userService.findById(rent.getUser_id()));
-        newRent.setPrice(rent.getPrice());
+        newRent.setPrice(existingCar.getPrice());
         newRent.setEnds_at(rent.getEnds_at());
-        newRent.setCar(carService.findById(rent.getCar_id()));
+        newRent.setCar(existingCar);
 
         rentRepository.save(newRent);
 
@@ -45,8 +55,23 @@ public class RentServImpl implements RentServ {
     }
 
     @Override
-    public Rent update(Integer id, RentDto user) {
-        return null;
+    public Rent update(Integer id) {
+        Rent targetRent = rentRepository.findById(id).orElse(null);
+        User targetUser = targetRent.getUser();
+        Car targetCar = targetRent.getCar();
+        LocalDate skrg = LocalDate.now();
+
+        if (targetRent.getEnds_at().isBefore(skrg)) {
+            targetUser.setBalance(targetUser.getBalance() - targetRent.getPrice());
+        } else if(targetRent.getEnds_at().isAfter(skrg)){
+            targetUser.setBalance(targetUser.getBalance() - targetRent.getPrice() + (targetRent.getPrice() * 10 / 100) );
+        }
+        targetRent.setCompleted(true);
+        targetCar.setAvailable(true);
+
+        userRepo.save(targetUser);
+        carRepo.save(targetCar);
+        return rentRepository.save(targetRent);
     }
 
     @Override
@@ -54,5 +79,3 @@ public class RentServImpl implements RentServ {
         rentRepository.deleteById(id);
     }
 }
-
-
