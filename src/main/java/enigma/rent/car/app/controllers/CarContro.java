51e92c2270cb5cf5fc08dcf595de8a3bc890ef5d
vigorjs <111201912289@mhs.dinus.ac.id>
2024-07-03 11:04:16 +0700

    feat: add responseWrapper

--- src/main/java/enigma/rent/car/app/controllers/CarContro.java
+++ src/main/java/enigma/rent/car/app/controllers/CarContro.java
@@ -3,7 +3,10 @@ package enigma.rent.car.app.controllers;
 import enigma.rent.car.app.models.Car;
 import enigma.rent.car.app.services.CarServ;
 import enigma.rent.car.app.utils.dto.CarDto;
+import enigma.rent.car.app.utils.responseWrapper.Res;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,28 +18,31 @@ public class CarContro {
     private final CarServ carServ;
 
     @GetMapping
-    public List<Car> findAll(){
-        return carServ.findAll();
+    public ResponseEntity<?> findAll(){
+        return Res.renderJson(carServ.findAll(), "KETEMU!!!", HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Car findById(@PathVariable Integer id){
-        return carServ.findById(id);
+    public ResponseEntity<?> findById(@PathVariable Integer id){
+
+        return Res.renderJson(carServ.findById(id), "KETEMU!!!", HttpStatus.OK);
     }
 
     @PostMapping
-    public Car create(@RequestBody CarDto car){
-        return carServ.create(car);
+    public ResponseEntity<?> create(@RequestBody CarDto car){
+
+        return Res.renderJson(carServ.create(car), "Berhasil di create!!!", HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public Car update(@PathVariable Integer id, @RequestBody CarDto car){
-        return carServ.update(id, car);
+    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody CarDto car){
+        return Res.renderJson(carServ.update(id, car), "Berhasil di update!!!", HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Integer id){
+    public ResponseEntity<?> delete(@PathVariable Integer id){
         carServ.deleteById(id);
+        return Res.renderJson(null, "Berhasil di delete!!!", HttpStatus.OK);
     }
 
 }
